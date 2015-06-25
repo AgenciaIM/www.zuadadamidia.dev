f41a2182e1fb37add936b03d6fcf65f8b0671345
mastheusum <victor.matheus.xd@gmail.com>
2015-06-25 11:34:24 -0300

    Formatação da página category.php

--- wp-content/themes/ZuadaDaMidia/category.php
+++ wp-content/themes/ZuadaDaMidia/category.php
@@ -1,26 +1,91 @@
 <?php
 	get_header();
 ?>
-
+	<!-- container geral -->
 	<div class="container">
-		<div class="col-lg-5">
-			<img src="<?php echo get_bloginfo('template_url'); ?>/play_video_device_web.png"/>
+		<div class="row">
+			<!--imágem da notícia -->
+			<div class="col-lg-9">
+				<div class="row">
+					<div class="col-lg-7">
+						<img src="<?php echo get_bloginfo('template_url'); ?>/play_video_device_web.png"/>
+					</div>
+					<!-- texto da notícias -->
+					<div class="col-lg-5">
+						<a href="#"><h3>Título da seiláoque</h3></a>
+						<a href="#"></a>
+							<p>
+								O trecho padrão original de Lorem Ipsum, usado 
+								desde o século XVI, está reproduzido abaixo para os 
+								interessados.
+							<p>
+						<div class="full-container" style="overflow:hidden;max-height:215px;box-shadow:0px 1px 0px rgba(200,200,200,0.5)">
+							<div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/ODiaAlagoas" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true" data-show-posts="false" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=&amp;container_width=285&amp;hide_cover=true&amp;href=https%3A%2F%2Fwww.facebook.com%2FODiaAlagoas&amp;locale=pt_BR&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;small_header=false">
+								<span style="vertical-align: bottom; width: 285px; height: 214px;">
+									<iframe name="f2e9c8ea08" width="1000px" height="1000px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:page Facebook Social Plugin" src="http://www.facebook.com/v2.3/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter%2F1ldYU13brY_.js%3Fversion%3D41%23cb%3Df3cf5ca6e4%26domain%3Dlocalhost%26origin%3Dhttp%253A%252F%252Flocalhost%252Ff3f5bab734%26relation%3Dparent.parent&amp;container_width=285&amp;hide_cover=true&amp;href=https%3A%2F%2Fwww.facebook.com%2FODiaAlagoas&amp;locale=pt_BR&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;small_header=false" style="border: none; visibility: visible; width: 285px; height: 214px;" class="">
+									</iframe>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- notícias mais antigas -->
+				<div class="row" style="padding:10px 0;">
+					<div class="col-lg-3">
+						<img src="http://noticiasdatv.uol.com.br/application/public/img/noticias/thumbs/t_180_105_1435179046Neila_Medeiros_Jornal_Do_SBT.jpg">
+					</div>
+					<div class="col-lg-8">
+						<a href="#"><h4><i>Título da segunda notícia</i></h4></a>
+						<a href="#">
+							<p>
+								O trecho padrão original de Lorem Ipsum, usado 
+								desde o século XVI, está reproduzido abaixo para os 
+								interessados.
+							</p>
+						</a>
+					</div>
+				</div>
+
+				<div class="row" style="padding:10px 0;">
+					<div class="col-lg-3">
+						<img src="http://noticiasdatv.uol.com.br/application/public/img/noticias/thumbs/t_180_105_1435179046Neila_Medeiros_Jornal_Do_SBT.jpg">
+					</div>
+					<div class="col-lg-8">
+						<a href="#"><h4><i>Título da terceira notícia</i></h4></a>
+						<a href="#">
+							<p>
+								O trecho padrão original de Lorem Ipsum, usado 
+								desde o século XVI, está reproduzido abaixo para os 
+								interessados.
+							</p>
+						</a>
+					</div>
+				</div>
+
+				<div class="row" style="padding:10px 0;">
+					<div class="col-lg-3">
+						<img src="http://noticiasdatv.uol.com.br/application/public/img/noticias/thumbs/t_180_105_1435179046Neila_Medeiros_Jornal_Do_SBT.jpg">
+					</div>
+					<div class="col-lg-8">
+						<a href="#"><h4><i>Título da quarta notícia</i></h4></a>
+						<a href="#">
+							<p>
+								O trecho padrão original de Lorem Ipsum, usado 
+								desde o século XVI, está reproduzido abaixo para os 
+								interessados.
+							</p>
+						</a>
+					</div>
+				</div>
+
+			</div>
+			<!-- publicidade -->
+			<div class="col-lg-3" style="padding: 10px 0;">
+				<div class="titulo-pub">Publicidade</div>
+				<img class="img-responsive" src="http://localhost/www.zuadadamidia.dev/wp-content/themes/ZuadaDaMidia/img/pub-250x520.jpg">
+			</div>
 		</div>
-		<div class="col-lg-4">
-			O trecho padrão original de Lorem Ipsum, usado 
-			desde o século XVI, está reproduzido abaixo para os 
-			interessados. Seções 1.10.32 e 1.10.33 de "de Finibus 
-			Bonorum et Malorum" de Cicero também foram 
-			reproduzidas abaixo em sua forma exata original, 
-			acompanhada das versões para o inglês da tradução 
-			feita por H. Rackham em 1914.
-		</div>
-		<!--
-		<div class="col-lg-5">
-			<div class="titulo-pub">Publicidade</div>
-			<img class="img-responsive" src="http://localhost/www.zuadadamidia.dev/wp-content/themes/ZuadaDaMidia/img/pub-250x520.jpg">
-		</div>
-		-->
+		
 	</div>
 
 <?php
